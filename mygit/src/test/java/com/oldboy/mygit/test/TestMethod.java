e6660abad80a504aa50a6877dcc0ffd19a6fa99f
oldboy-xupccc <770305805@qq.com>
2018-09-25 09:59:01 +0800

    ssss

---
+++ mygit/src/test/java/com/oldboy/mygit/test/TestMethod.java
@@ -0,0 +1,14 @@
+package com.oldboy.mygit.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/9/25.
+ */
+public class TestMethod {
+
+	@Test
+	public void test1(){
+		System.out.println("hello world");
+	}
+}
