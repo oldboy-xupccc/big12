23a506328882c442dd978ff2796cdf284602b528
oldboy-xupccc <770305805@qq.com>
2018-09-25 10:08:54 +0800

    ssss

--- mygit/src/test/java/com/oldboy/mygit/test/TestMethod.java
+++ mygit/src/test/java/com/oldboy/mygit/test/TestMethod.java
@@ -3,12 +3,15 @@ package com.oldboy.mygit.test;
 import org.junit.Test;
 
 /**
- * Created by Administrator on 2018/9/25.
+ * 测试git。
  */
 public class TestMethod {
 
 	@Test
 	public void test1(){
 		System.out.println("hello world");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
+		System.out.println("hello world4");
 	}
 }
